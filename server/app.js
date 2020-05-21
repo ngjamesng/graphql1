411882031809b0ff08f74b17dbc765ac28513e93
ngjamesng <just.james.ng@gmail.com>
2020-05-21 09:52:40 -0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
