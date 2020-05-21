71fef044a326409cfc8ad53b13bfc9469f643f03
ngjamesng <just.james.ng@gmail.com>
2020-05-21 10:34:00 -0700

    add resolve return statement

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -2,29 +2,36 @@ const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// dummy data
+let books = [
+	{ name: 'Name of the Wind', genre: 'Fantasy', id: '1' },
+	{ name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+	{ name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
+];
+
 const BookType = new GraphQLObjectType({
-    name: 'Book',
-    fields: ( ) => ({
-        id: { type: GraphQLString },
-        name: { type: GraphQLString },
-        genre: { type: GraphQLString }
-    })
+	name: 'Book',
+	fields: () => ({
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		genre: { type: GraphQLString }
+	})
 });
 
 const RootQuery = new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-        book: {
-            type: BookType,
-            args: { id: { type: GraphQLString } },
-            resolve(parent, args){
-                // code to get data from db / other source
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			type: BookType,
+			args: { id: { type: GraphQLString } },
+			resolve(parent, args) {
+				// code to get data from db / other source
 
-            }
-        }
-    }
+			}
+		}
+	}
 });
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+	query: RootQuery
 });
