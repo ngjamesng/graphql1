9b51d2f2948882454834cfed2e7a9a58d2ef92a8
ngjamesng <just.james.ng@gmail.com>
2020-05-22 18:51:29 -0700

    add mongoose, connect mongoose to db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
+require("dotenv").config(); //enables usage of environment variables
+
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require("mongoose");
 
 const app = express();
+
+const { MONGODB_USERNAME, MONGODB_PASSWORD } = process.env;
+mongoose.connect(`mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@gql-ngjamesng-slvid.mongodb.net/test?retryWrites=true&w=majority`,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
 
 // bind express with graphql
 app.use('/graphql', graphqlHTTP({
