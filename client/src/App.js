7e016d40674c51fd7fabb6230ab4739b22089c89
ngjamesng <just.james.ng@gmail.com>
2020-05-26 08:54:37 -0700

    add apollo to frontend, wrap app in ApolloProvider with client

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
 import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Books & Authors</h1>
-      <BookList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Books & Authors</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
