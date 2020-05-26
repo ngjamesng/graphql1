e7d813f2f6d3e1bae411a5dbd2408dd16e3b3353
ngjamesng <just.james.ng@gmail.com>
2020-05-26 08:35:38 -0700

    add BookList component basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import BookList from "./components/BookList";
 
 function App() {
   return (
     <div className="App">
       <h1>Books & Authors</h1>
+      <BookList />
     </div>
   );
 }
---
+++ client/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book 1 name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
