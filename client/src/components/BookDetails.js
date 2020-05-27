0a9f7589048beaa227c4b08a9fd172a1f02fe219
ngjamesng <just.james.ng@gmail.com>
2020-05-27 09:01:57 -0700

    add displayBook for single book details

--- client/src/components/BookDetails.js
+++ client/src/components/BookDetails.js
@@ -3,16 +3,27 @@ import { useQuery } from "@apollo/react-hooks";
 import { getBookQuery } from "../queries/queries";
 
 
-function BookDetails({ bookId }) {
+function BookDetails({ bookId: id }) {
   const { loading, error, data } = useQuery(getBookQuery, {
-    variables: { id: bookId }
+    variables: { id }
   });
 
+  const displayBook = () => (
+    !loading && data.book
+      ?
+      <div>
+        <h2> {data.book.name}</h2>
+        <p> {data.book.genre}</p>
+        <p> other books by {data.book.author.name}</p>
+        <ul className="other-books">
+          {data.book.author.books.map(book => (<li key={book.id}> {book.name}</li>))}
+        </ul>
+      </div>
+      : <div> No book selected... </div>
+  )
   return (
     <div id="book-details">
-      <p>
-        book details here
-      </p>
+      {displayBook()}
     </div>
   )
 }
