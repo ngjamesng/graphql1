13fbb5de201f6227dd849f3fb18a9c2185a69b7e
ngjamesng <just.james.ng@gmail.com>
2020-05-26 20:22:16 -0700

    add formData state and handleChange for form

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,35 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from "@apollo/react-hooks";
 import { getAuthorsQuery } from "../queries/queries";
 
 
 function AddBook() {
   const { loading, error, data } = useQuery(getAuthorsQuery);
+  const [formData, setFormData] = useState({
+    name: "",
+    genre: "",
+    authorId: ""
+  })
 
   const displayAUthors = () => (
     loading
       ? <option disabled>loading authors... </option>
       : data.authors.map(author => (<option key={author.id} value={author.id}>{author.name}</option>))
   )
+
+  const handleChange = (evt) => {
+    const { name, value } = evt.target;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value
+    }))
+  }
+
+  const submitForm = (evt) => {
+    evt.preventDefault();
+    console.log("SUBMITTED,", evt)
+    console.log(formData);
+  }
   return (
-    <form>
+    <form onSubmit={(e) => submitForm(e)} onChange={handleChange}>
       <div className="field">
         <label>
           Book Name:
         </label>
-        <input type="text" />
+        <input type="text" name="name"/>
       </div>
       <div className="field">
         <label>
           Genre:
         </label>
-        <input type="text" />
+        <input type="text" name="genre"/>
       </div>
       <div className="field">
         <label>
           Author:
         </label>
-        <select>
+        <select name="authorId">
           <option> select author </option>
           {displayAUthors()}
         </select>
